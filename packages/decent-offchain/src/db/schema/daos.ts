82e4e05149367512e54c65f1cee25c5aad58eb4f
jigglyjams <jigglyjams@protonmail.com>
2025-03-13 23:52:39 -0400

    fix(schema): hack dao schema into offchain folder

---
+++ packages/decent-offchain/src/db/schema/daos.ts
@@ -0,0 +1,17 @@
+import { integer, text, boolean, pgSchema } from "drizzle-orm/pg-core";
+import { onchainSchema } from "./common";
+
+// copy of onchain schema from @decent-ponder/ponder.schema.ts
+// TODO: remove this once we have a way to sync the schema
+export const daos = onchainSchema.table("daos", {
+  dao: text().primaryKey(),
+  daoName: text(),
+  proposalTemplatesCID: text(),
+  snapshotENS: text(),
+  subDaoOf: text(),
+  topHatId: text(),
+  hatIdToStreamId: text(),
+  gaslessVotingEnabled: boolean(),
+  createdAt: integer(),
+  updatedAt: integer(),
+}); 
--- packages/decent-offchain/src/db/schema/proposals.ts
+++ packages/decent-offchain/src/db/schema/proposals.ts
@@ -3,17 +3,17 @@ import { integer, json, text, timestamp, varchar } from "drizzle-orm/pg-core";
 import { offchainSchema } from "./common";
 
 export const proposals = offchainSchema.table("proposals", {
-  slug: varchar("slug").primaryKey().unique().default(nanoid()),
-  dao: varchar("dao").notNull(),
-  authorAddress: varchar("author_address").notNull(),
-  status: varchar("status"),
-  cycle: integer("cycle"),
-  id: integer("id"), // number to concat with organization prefix (ex: DCT-1)
-  safeTxHash: varchar("safe_tx_hash"),
-  title: text("title"),
-  body: text("body"),
-  voteType: varchar("vote_type"),
-  voteChoices: json("vote_choices").$type<string[]>(),
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at").defaultNow(),
+  slug: varchar().primaryKey().unique().default(nanoid()),
+  dao: text().notNull(),
+  authorAddress: varchar().notNull(),
+  status: varchar(),
+  cycle: integer(),
+  id: integer(), // number to concat with organization prefix (ex: DCT-1)
+  safeTxHash: varchar(),
+  title: text(),
+  body: text(),
+  voteType: varchar(),
+  voteChoices: json().$type<string[]>(),
+  createdAt: timestamp().defaultNow(),
+  updatedAt: timestamp().defaultNow(),
 });
