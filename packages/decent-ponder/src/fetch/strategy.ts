70500f29b879d3b3d5e12edabb9540782d17bb7a
jigglyjams <jigglyjams@protonmail.com>
2025-03-20 23:06:34 -0400

    feat(tokens): find tokens through series of contract interactions

---
+++ packages/decent-ponder/src/fetch/strategy.ts
@@ -0,0 +1,57 @@
+import { Context } from "ponder:registry";
+import { Address } from "viem";
+import { abis } from "@fractal-framework/fractal-contracts";
+import {
+  getPages,
+  SENTINEL_ADDRESS,
+  PAGE_SIZE,
+} from "./common";
+
+export async function getStrategy(context: Context, moduleAddress: Address) {
+  try { 
+    const [
+      _domainSeparatorTypeHash,
+      _transactionTypeHash,
+      avatar,
+      strategiesResponse
+    ] = await context.client.multicall({
+      contracts: [
+        {
+          abi: abis.Azorius,
+          address: moduleAddress,
+          functionName: "DOMAIN_SEPARATOR_TYPEHASH",
+        },
+        {
+          abi: abis.Azorius,
+          address: moduleAddress,
+          functionName: "TRANSACTION_TYPEHASH",
+        },
+        {
+          abi: abis.Azorius,
+          address: moduleAddress,
+          functionName: "avatar",
+        },
+        {
+          abi: abis.Azorius,
+          address: moduleAddress,
+          functionName: "getStrategies",
+          args: [SENTINEL_ADDRESS, PAGE_SIZE],
+        },
+      ],
+      allowFailure: false,
+    });
+
+    const strategies: Address[] = [];
+    if (strategiesResponse[0].length < PAGE_SIZE) {
+      strategies.push(...strategiesResponse[0]);
+    } else {
+      const moreStrategies = await getPages(context, moduleAddress, "Azorius", "getStrategies");
+      strategies.push(...moreStrategies);
+    }
+
+    return strategies;
+  } catch (error) {
+    // if multicall fails, it's probably not an Azorius module
+    return null;
+  }
+}
--- packages/decent-ponder/src/index.ts
+++ packages/decent-ponder/src/index.ts
@@ -1,69 +1,84 @@
-import { Context, ponder } from "ponder:registry";
+import { Context, Event, ponder } from "ponder:registry";
 import { daos, DaoInsert } from "ponder:schema";
 import { chainIdToPrefix } from "./networks";
-import { fetchSafe } from "./safe/fetch";
+import { fetchGovernance } from "./fetch";
+import { Address } from "viem";
+
+const getGovernanceFormatEntry = async (
+  event: Event,
+  context: Context,
+  safeAddress: Address
+): Promise<DaoInsert> => {
+  const timestamp = Number(event.block.timestamp);
+  const prefix = chainIdToPrefix(context.network.chainId);
+  const dao = `${prefix}:${safeAddress}`;
+  const entry: DaoInsert = { dao, createdAt: timestamp };
+  const daoInfo = await context.db.find(daos, { dao });
+
+  if (!daoInfo?.signers) {
+    const governance = await fetchGovernance(context, safeAddress);
+    entry.azoriusModuleAddress = governance.strategy;
+    entry.signers = governance.owners;
+    entry.requiredSignatures = governance.threshold;
+    entry.votingStrategyAddress = governance.strategy;
+    entry.votingTokenType = governance.token?.type;
+    entry.votingTokenAddress = governance.token?.addresses;
+  }
+
+  return entry;
+};
 
 // KeyValuePairs is a generic key value store for Decent
 // Subgraph: https://github.com/decentdao/decent-subgraph/blob/main/src/key-value-pairs.ts
 // Contract: https://github.com/decentdao/decent-contracts/blob/develop/contracts/singletons/KeyValuePairs.sol
 ponder.on("KeyValuePairs:ValueUpdated", async ({ event, context }) => {
   const { theAddress: safeAddress, key, value } = event.args;
-  const timestamp = Number(event.block.timestamp);
-  const prefix = chainIdToPrefix(context.network.chainId);
-  const dao = `${prefix}:${safeAddress}`
-  const entry: DaoInsert = { dao, createdAt: timestamp };
-
-  const daoInfo = await context.db.find(daos, { dao });
-  if (!daoInfo?.signers) {
-    const daoInfo = await fetchSafe(context, safeAddress);
-    entry.azoriusModuleAddress = daoInfo?.modules[0];
-    entry.signers = daoInfo?.owners;
-    entry.requiredSignatures = daoInfo?.threshold;
-  }
-
-
+  const entry = await getGovernanceFormatEntry(event, context, safeAddress);
+  const updatedAt = entry.createdAt;
   if (key === "daoName") {
     await context.db.insert(daos)
       .values({ ...entry, daoName: value })
-      .onConflictDoUpdate({ daoName: value, updatedAt: timestamp });
+      .onConflictDoUpdate({ daoName: value, updatedAt });
   
   } else if (key === "proposalTemplates") {
     await context.db.insert(daos)
       .values({ ...entry, proposalTemplatesCID: value })
-      .onConflictDoUpdate({ proposalTemplatesCID: value, updatedAt: timestamp });
+      .onConflictDoUpdate({ proposalTemplatesCID: value, updatedAt });
   
   } else if (key === "snapshotENS" || key === "snapshotURL") {
     const cleanedValue = value === "" ? null : value;
     await context.db.insert(daos)
       .values({ ...entry, snapshotENS: cleanedValue })
-      .onConflictDoUpdate({ snapshotENS: cleanedValue, updatedAt: timestamp });
+      .onConflictDoUpdate({ snapshotENS: cleanedValue, updatedAt });
   
   } else if (key === "childDao") {
-    entry.dao = `${prefix}:${value}`;
+    const prefix = chainIdToPrefix(context.network.chainId);
+    const subDaoOf = entry.dao;
+    entry.dao = `${prefix}:${value}`
     await context.db.insert(daos)
-      .values({ ...entry, subDaoOf: dao })
+      .values({ ...entry, subDaoOf })
       .onConflictDoNothing();
 
   } else if (key === "topHatId") {
     await context.db.insert(daos)
       .values({ ...entry, topHatId: value })
-      .onConflictDoUpdate({ topHatId: value, updatedAt: timestamp });
+      .onConflictDoUpdate({ topHatId: value, updatedAt });
   
   } else if (key === "hatIdToStreamId") {
     await context.db.insert(daos)
       .values({ ...entry, hatIdToStreamId: value })
-      .onConflictDoUpdate({ hatIdToStreamId: value, updatedAt: timestamp });
+      .onConflictDoUpdate({ hatIdToStreamId: value, updatedAt });
   
   } else if (key === "gaslessVotingEnabled") {
     await context.db.insert(daos)
       .values({ ...entry, gaslessVotingEnabled: value === "true" })
-      .onConflictDoUpdate({ gaslessVotingEnabled: value === "true", updatedAt: timestamp });
+      .onConflictDoUpdate({ gaslessVotingEnabled: value === "true", updatedAt });
     
   } else {
     console.log("--------------------------------");
     console.log("Unknown key:", key);
     console.log("Network:", context.network.chainId);
-    console.log("DAO:", dao);
+    console.log("DAO:", entry.dao);
     console.log("Value:", value);
     console.log("--------------------------------");
   }
@@ -74,40 +89,21 @@ ponder.on("KeyValuePairs:ValueUpdated", async ({ event, context }) => {
 // Contract: https://github.com/decentdao/decent-contracts/blob/87b74fc69c788709bb606c59e41cf5a365506b06/contracts/FractalRegistry.sol
 ponder.on("FractalRegistry:FractalNameUpdated", async ({ event, context }) => {
   const { daoAddress, daoName } = event.args;
-  const timestamp = Number(event.block.timestamp);
-  const prefix = chainIdToPrefix(context.network.chainId);
-  const dao = `${prefix}:${daoAddress}`;
-  const entry: DaoInsert = { dao, createdAt: timestamp };
-
-  const daoInfo = await context.db.find(daos, { dao });
-  if (!daoInfo?.signers) {
-    const daoInfo = await fetchSafe(context, daoAddress);
-    entry.azoriusModuleAddress = daoInfo?.modules[0];
-    entry.signers = daoInfo?.owners;
-    entry.requiredSignatures = daoInfo?.threshold;
-  }
+  const entry = await getGovernanceFormatEntry(event, context, daoAddress);
+  const updatedAt = entry.createdAt;
 
   await context.db.insert(daos)
     .values({ ...entry, daoName })
-    .onConflictDoUpdate({ daoName, updatedAt: timestamp });
+    .onConflictDoUpdate({ daoName, updatedAt });
 });
 
 ponder.on("FractalRegistry:FractalSubDAODeclared", async ({ event, context }) => {
   const { parentDAOAddress, subDAOAddress } = event.args;
-  const timestamp = Number(event.block.timestamp);
+  const entry = await getGovernanceFormatEntry(event, context, subDAOAddress);
+  const updatedAt = entry.createdAt;
   const prefix = chainIdToPrefix(context.network.chainId);
-  const dao = `${prefix}:${subDAOAddress}`;
-  const entry: DaoInsert = { dao, createdAt: timestamp };
-
-  const daoInfo = await context.db.find(daos, { dao });
-  if (!daoInfo?.signers) {
-    const daoInfo = await fetchSafe(context, subDAOAddress);
-    entry.azoriusModuleAddress = daoInfo?.modules[0];
-    entry.signers = daoInfo?.owners;
-    entry.requiredSignatures = daoInfo?.threshold;
-  }
 
   await context.db.insert(daos)
     .values({ ...entry, subDaoOf: `${prefix}:${parentDAOAddress}` })
-    .onConflictDoUpdate({ subDaoOf: `${prefix}:${parentDAOAddress}`, updatedAt: timestamp });
+    .onConflictDoUpdate({ subDaoOf: `${prefix}:${parentDAOAddress}`, updatedAt });
 });
