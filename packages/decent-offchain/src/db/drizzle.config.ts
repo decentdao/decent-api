a5c7bc9ee4844f5ac918bfe0c2d0f8fa6679ea32
jigglyjams <jigglyjams@protonmail.com>
2025-03-28 16:05:24 -0400

    no star im confused

--- packages/decent-offchain/src/db/drizzle.config.ts
+++ packages/decent-offchain/src/db/drizzle.config.ts
@@ -3,12 +3,12 @@ import { connectionString } from ".";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: [
-    "./src/db/schema/offchain/*",
-  ],
+  schema: "./src/db/schema/offchain",
   out: "./drizzle",
   dbCredentials: {
     url: connectionString,
   },
   casing: "snake_case",
+  verbose: true,
+  schemaFilter: ["offchain"],
 });
