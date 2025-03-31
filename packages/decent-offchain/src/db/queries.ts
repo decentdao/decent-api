09440477225d6aaba484a19cc8470dcbb7b88c6c
jigglyjams <jigglyjams@protonmail.com>
2025-03-31 15:26:13 -0400

    reuse expanded DAO with calls, update signerstodao table, add eslint to decent-offchain

---
+++ packages/decent-offchain/src/db/queries.ts
@@ -0,0 +1,32 @@
+export const DEFAULT_DAO_WITH = {
+  governanceModules: {
+    columns: {
+      address: true,
+      name: true,
+      description: true,
+    },
+    with: {
+      votingStrategies: {
+        columns: {
+          address: true,
+          minProposerBalance: true,
+          name: true,
+          description: true,
+        },
+        with: {
+          votingTokens: {
+            columns: {
+              address: true,
+              type: true,
+            },
+          },
+        },
+      },
+    },
+  },
+  signers: {
+    columns: {
+      address: true,
+    },
+  },
+};
