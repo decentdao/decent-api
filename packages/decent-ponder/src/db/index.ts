b69cb8b109626535d9f0aa481e805a70d2349535
jigglyjams <jigglyjams@protonmail.com>
2025-04-02 11:27:46 -0400

    fix(ponder): remove unused db stuff

--- packages/decent-ponder/src/db/index.ts
+++
@@ -1,6 +0,0 @@
-import { Context } from "ponder:registry";
-import { Dao } from "ponder:schema";
-
-export async function handleDao(context: Context, dao: Dao) {
-  const { address, chainId } = dao;
-}
