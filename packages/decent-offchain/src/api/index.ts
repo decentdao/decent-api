d2d79a1b5f8eade15b3e554d0b1e8df5f33a1700
jigglyjams <jigglyjams@protonmail.com>
2025-04-30 14:39:53 -0400

    fix(ws): move up in routes so everybody sees it early

--- packages/decent-offchain/src/api/index.ts
+++ packages/decent-offchain/src/api/index.ts
@@ -5,12 +5,12 @@ import resf from '@/api/utils/responseFormatter';
 
 // Route imports
 import meta from '@/api/routes/meta';
+import socket from '@/api/ws/socket';
 import docs from '@/api/routes/docs';
 import auth from '@/api/routes/auth';
 import dao from '@/api/routes/dao';
 import proposals from '@/api/routes/dao.proposals';
 import comments from '@/api/routes/dao.comments';
-import socket from '@/api/ws/socket';
 
 const app = new Hono();
 const { websocket } = createBunWebSocket();
@@ -29,12 +29,12 @@ app.onError((err, c) => {
 
 // Routes
 app.route('/', meta);
+app.route('/ws', socket);
 app.route('/docs', docs);
 app.route('/auth', auth);
 app.route('/d', dao);
 app.route('/d/:chainId/:address/proposals', proposals);
 app.route('/d/:chainId/:address/proposals/:slug/comments', comments);
-app.route('/ws', socket);
 
 export default {
   port: 3005,
