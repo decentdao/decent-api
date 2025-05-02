da23a1d01bad32447ce514d151ec32013d4bd529
jigglyjams <jigglyjams@protonmail.com>
2025-05-02 18:29:21 -0400

    fix(cookie): domain for safari

--- packages/decent-offchain/src/api/utils/cookie.ts
+++ packages/decent-offchain/src/api/utils/cookie.ts
@@ -3,6 +3,7 @@ export const cookieName = 'decent-session';
 export const cookieOptions = {
   httpOnly: true,
   maxAge: 60 * 60 * 24 * 7,
-  secure: process.env.NODE_ENV === 'production',
+  secure: true,
   sameSite: 'none',
+  domain: process.env.NODE_ENV === 'production' ? 'decentdao.org' : 'localhost',
 } as const;
