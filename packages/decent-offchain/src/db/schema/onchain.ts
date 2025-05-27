b5a864e02cc6549a951f456d139f0c5edc871199
jigglyjams <jigglyjams@protonmail.com>
2025-05-27 07:54:35 -0400

    fix(schema): remove many proposals from dao schema

--- packages/decent-offchain/src/db/schema/onchain.ts
+++ packages/decent-offchain/src/db/schema/onchain.ts
@@ -113,7 +113,6 @@ export const daoTableRelations = relations(daoTable, ({ many }) => ({
   signers: many(signerToDaoTable),
   governanceModules: many(governanceModuleTable),
   hatIdToStreamIds: many(hatIdToStreamIdTable),
-  // proposals: many(onchainProposalTable),
 }));
 
 export const governanceModuleTableRelations = relations(governanceModuleTable, ({ one, many }) => ({
