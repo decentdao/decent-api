80910c003490060190f981e95c0fca59b6f2fc20
jigglyjams <jigglyjams@protonmail.com>
2025-09-03 15:07:33 -0400

    test: fix simple test

--- packages/decent-offchain/test/dao.data.ts
+++ packages/decent-offchain/test/dao.data.ts
@@ -5,7 +5,6 @@ import {
   governanceModuleTable,
   votingStrategyTable,
   votingTokenTable,
-  signerToDaoTable,
   DbDao,
 } from '../src/db/schema/onchain';
 import { daoAddress, daoChainId } from './constants';
@@ -19,19 +18,7 @@ async function insertDaoData() {
         chainId: daoChainId,
         address: daoAddress,
         name: 'decent-api',
-        requiredSignatures: 1,
-        fractalModuleAddress: '0x0000000000000000000000000000000000000000',
         createdAt: 1744234225,
-      })
-      .onConflictDoNothing();
-
-    // Insert Signer to DAO relationship
-    await db
-      .insert(signerToDaoTable)
-      .values({
-        address: '0xd0cbdf4b48d3abc9cc3df373ed2f0f91a38c954c',
-        daoChainId: daoChainId,
-        daoAddress: daoAddress,
       })
       .onConflictDoNothing();
 
@@ -51,7 +38,7 @@ async function insertDaoData() {
       .values({
         address: '0xe6d3d6c70f22459a98985a998f1a1ddf0949e0d0',
         governanceModuleId: '0xd0cbdf4b48d3abc9cc3df373ed2f0f91a38c954c',
-        minProposerBalance: '1',
+        requiredProposerWeight: 1,
       })
       .onConflictDoNothing();
 
@@ -69,7 +56,7 @@ async function insertDaoData() {
     const dao = (await db.query.daoTable.findFirst({
       where: (dao, { eq, and }) => and(eq(dao.chainId, daoChainId), eq(dao.address, daoAddress)),
       with: DEFAULT_DAO_WITH,
-    })) as DbDao;
+    })) as DbDao | undefined;
     console.log(dao);
   } catch (error) {
     console.error('Error inserting DAO data:', error);
