ed46d28df19880dc9e6cc0e11735dd5f9ace78e4
jigglyjams <jigglyjams@protonmail.com>
2025-09-26 00:07:04 -0400

    style: pretty

--- packages/decent-offchain/src/db/schema/offchain/kyc.ts
+++ packages/decent-offchain/src/db/schema/offchain/kyc.ts
@@ -4,18 +4,26 @@ import { offchainSchema } from './offchain';
 import { hex } from '../hex';
 import { timestamps } from '../timestamps';
 
-export const REVIEW_STATUSES = ['init', 'pending', 'completed', 'onHold', 'awaitingService'] as const;
-export type ReviewStatus = typeof REVIEW_STATUSES[number];
+export const REVIEW_STATUSES = [
+  'init',
+  'pending',
+  'completed',
+  'onHold',
+  'awaitingService',
+] as const;
+export type ReviewStatus = (typeof REVIEW_STATUSES)[number];
 
-export const kycTable = offchainSchema.table('kyc', {
-  id: text('id')
-    .primaryKey()
-    .$defaultFn(() => nanoid()),
-  address: hex().notNull(),
-  applicantId: text('applicant_id'),
-  reviewStatus: text('review_status').default('init').$type<ReviewStatus>(),
-  isKycApproved: boolean().notNull().default(false),
-  ...timestamps,
-}, (table) => [
-  index('kyc_address_idx').on(table.address),
-]);
+export const kycTable = offchainSchema.table(
+  'kyc',
+  {
+    id: text('id')
+      .primaryKey()
+      .$defaultFn(() => nanoid()),
+    address: hex().notNull(),
+    applicantId: text('applicant_id'),
+    reviewStatus: text('review_status').default('init').$type<ReviewStatus>(),
+    isKycApproved: boolean().notNull().default(false),
+    ...timestamps,
+  },
+  table => [index('kyc_address_idx').on(table.address)],
+);
